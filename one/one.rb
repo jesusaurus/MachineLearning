521ac63fc0382d3b20ed15eb66f6d93d82a590c8
K Jonathan Harker <kjharke@cs.pdx.edu>
2012-01-23 11:25:22 -0800

    Results for Experiment 4

--- one/one.rb
+++ one/one.rb
@@ -7,7 +7,7 @@ class Perceptron
         @testing     = Hash.new #testing data
         @features    = 64
         @w           = Array.new(@features + 1) {|i| rand * 2 - 1} #weights: -1 <= w <= 1
-        @eta         = 0.2
+        @eta         = 0.5
         @accuracy    = 0.0
         @epochs      = 0 #how many epochs have we trained on so far
         @sourceClass = source #the class we want to match
