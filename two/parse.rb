4866c7e56dd4660744b42d9c2f43ca28000a8eec
K Jonathan Harker <kjharke@cs.pdx.edu>
2012-02-07 14:38:44 -0800

    Added outputs and such.

--- two/parse.rb
+++ two/parse.rb
@@ -5,15 +5,20 @@ neg = 0
 
 File.open("spam.test", 'r') do |file|
     while (line = file.gets)
-	if (line.split.first.to_i > 0)
-	  pos = pos + 1
-	else
-	  neg = neg + 1
-	end
+        if (line.split.first.to_i > 0)
+            pos = pos + 1
+        else
+            neg = neg + 1
+        end
     end
 end
 
 ARGV.each do |arg|
+
+    puts
+    puts
+    puts " *** #{arg} *** "
+    puts
 
     tp = 0 #true positive
     tn = 0 #true negative
@@ -22,24 +27,32 @@ ARGV.each do |arg|
     count = 0
 
     File.open(arg, 'r') do |file|
-	while (line = file.gets)
-	    tmp = line.split.first.to_i
-	    if (tmp >= 0 && count < pos)
-		tp = tp + 1
-	    elsif (tmp >= 0)
-		fp = fp + 1
-	    elsif (count < pos)
-		fn = fn + 1
-	    else
-		tn = tn + 1
-	    end
-	    count = count + 1
-	end
+        while (line = file.gets)
+            tmp = line.split.first.to_f
+
+            if (tmp >= 0 && count < pos)
+                tp = tp + 1
+            elsif (count < pos)
+                fn = fn + 1
+            elsif (tmp >= 0)
+                fp = fp + 1
+            else
+                tn = tn + 1
+            end
+
+            count = count + 1
+        end
     end
 
-    puts "TP: " + "#{tp}"
-    puts "FP: " + "#{fp}"
-    puts "TN: " + "#{tn}"
-    puts "FN: " + "#{fn}"
+    puts "TP: #{tp}"
+    puts "FP: #{fp}"
+    puts "TN: #{tn}"
+    puts "FN: #{fn}"
+    puts
+    puts "Accuracy: #{(tp + tn).to_f / (tp + tn + fp + fn)}"
+    puts "Precision: #{tp.to_f / (tp + fp)}"
+    puts "Recall: #{tp.to_f / (tp + fn)}"
+    puts "True Positive Rate: #{tp.to_f / (tp + fn)}"
+    puts "False Positive Rate: #{fp.to_f / (fp + tn)}"
 end
 
