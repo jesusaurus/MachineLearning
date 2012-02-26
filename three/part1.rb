a58fe9a351da21faa91e19e5a1c545fa8005d62c
K Jonathan Harker <kjharke@cs.pdx.edu>
2012-02-26 00:01:21 -0800

    Why are spam.* files empty?

--- three/part1.rb
+++ three/part1.rb
@@ -6,10 +6,10 @@
 
 $data = Hash.new
 $features = 57
-$fold = .1
+$fold = 0.1
 
 #each line of the file contains 57 csv inputs, the 58th integer is the class
-File.open('../spambase/spambase.data').readlines.each do |line|
+File.open('spambase.data').readlines.each do |line|
     tmp = line.chomp.split(',').map(&:to_i)
     if $data[tmp.last].nil?
         $data[tmp.last] = []
@@ -17,9 +17,12 @@ File.open('../spambase/spambase.data').readlines.each do |line|
     $data[tmp.last] << tmp[0,$features]
 end
 
+puts "#{$data[0].size}"
+puts "#{$data[1].size}"
+
 $total = 0
-$data.each do |x|
-    $total += x.size
+$data.each do |k, v|
+    $total += v.size
 end
 
 $weights = Array.new($total, 1.0 / $total)
@@ -27,16 +30,28 @@ $weights = Array.new($total, 1.0 / $total)
 $training = Array.new
 $validation = Array.new
 
-$data.each do |d|
+$data.each do |c, datum|
     skip = 0
-    (1..(d.size/$total*$fold)).each do |x|
+    (1..(datum.size/$total*$fold)).each do |x|
         tmp = rand * 8 + 1
         (skip..skip+tmp-1).each do |i|
-            $training = d[i]
+            $training << datum[i]
         end
         skip += tmp
-        $validation << d[skip]
+        $validation << datum[skip]
     end
 end
 
-puts "#{$total}: #{$training.size} + #{$validation.size}\n"
+File.open('spam.train', 'w') do |file|
+    $training.each do |t|
+        file.write(t)
+        puts t.inspect
+    end
+end
+File.open('spam.val', 'w') do |file|
+    $validation.each do |v|
+        file.write(v)
+    end
+end
+
+output = `c4.5 ` 
