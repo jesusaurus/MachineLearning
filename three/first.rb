e20c06fbce52952bf7035c5fa1dfcd98408a85b3
K Jonathan Harker <kjharke@cs.pdx.edu>
2012-02-26 18:26:38 -0800

    I was missing a step.

--- three/first.rb
+++ three/first.rb
@@ -9,7 +9,7 @@ $features = 57
 $fold = 0.1
 
 #each line of the file contains 57 csv inputs, the 58th integer is the class
-File.open('spam.data').readlines.each do |line|
+File.open('spambase.data').readlines.each do |line|
     tmp = line.chomp.split(',').map(&:to_i)
     if $data[tmp.last].nil?
         $data[tmp.last] = []
@@ -26,29 +26,21 @@ $training = Array.new
 $validation = Array.new
 
 $data.each do |c, datum|
-    skip = 0
-    range = $fold * datum.size
-    (1..range).each do |x|
-        tmp = (rand * 8 + 1).to_i
-        (skip..skip+tmp-1).each do |i|
-            $training << (datum[i] + [c])
-        end
-        skip += tmp
-        $validation << (datum[skip] + [c])
+    (0..(datum.size-2)).step(2) do |d|
+        $training << (datum[d] + [c])
+        $validation << (datum[d+1] + [c])
     end
 end
 
-File.open('kfold.data', 'w') do |file|
+File.open('spam.data', 'w') do |file|
     $training.each do |t|
         file.write(t.join(','))
         file.write("\n")
     end
 end
-File.open('kfold.test', 'w') do |file|
+File.open('spam.test', 'w') do |file|
     $validation.each do |v|
         file.write(v.join(','))
         file.write("\n")
     end
 end
-
-system('c4.5 -f kfold -u | tail -n 23')
