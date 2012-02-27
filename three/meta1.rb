763c4b43894ab144b307959b6338b04097e777d2
K Jonathan Harker <kjharke@cs.pdx.edu>
2012-02-26 17:15:39 -0800

    Calculate averages

--- three/meta1.rb
+++ three/meta1.rb
@@ -6,6 +6,9 @@
 
 $reps = 10
 
+$test = Array.new
+$train = Array.new
+
 (1..$reps).each do |rep|
     puts
     puts "### Iteration #{rep} ###"
@@ -13,5 +16,28 @@ $reps = 10
 
     output = `./part1.rb`
     print output
+
+    File.open('spam.trainacc').readlines.each do |t|
+        $train << t.to_f
+    end
+
+    File.open('spam.testacc').readlines.each do |t|
+        $test << t.to_f
+    end
 end
 
+sum = 0
+$test.map {|t| sum += t}
+avgTest = sum / $test.size
+
+sum = 0
+$train.map {|t| sum += t}
+avgTrain = sum / $train.size
+
+puts
+puts "### Summary ###"
+puts
+puts "Average Training Accuracy: #{avgTrain.to_s}"
+puts
+puts "Average Testing Accuracy: #{avgTest.to_s}"
+puts
